bd546b7f16ff7ad7b6c48e5d60823f63e493673a
Crypt Kitty Dev <cryptkittydev@gmail.com>
2020-09-17 17:12:51 -0700

    minor formatting. added a print function

--- objects/creating_objects.php
+++ objects/creating_objects.php
@@ -1,6 +1,6 @@
 <?php
 
-$soup = new stdClass();   //creates object
+$soup = new stdClass();   //creates standard class object
 
 //lets give it some properties:
 $soup->name = "Butternut";
@@ -11,8 +11,8 @@ $soup->dates_eaten = ["2015/10/14", "2016/02/01", "2016/02/02"];
 
 var_dump($soup); //lets take a look at the object
 
-
-
+$print $soup->name . "is the soup we are having.<br />";
 
 /*this object is a very simple standard class object. 
+in reality, you may want to just create an associative array.
 it does not include any full blown OOP features. */
