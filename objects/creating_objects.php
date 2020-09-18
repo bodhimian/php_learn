c74ec942e32781d6360430799bb49af3dbe86c68
Crypt Kitty Dev <cryptkittydev@gmail.com>
2020-09-17 17:14:16 -0700

    fixed print to not be a variable but be a func

--- objects/creating_objects.php
+++ objects/creating_objects.php
@@ -11,7 +11,7 @@ $soup->dates_eaten = ["2015/10/14", "2016/02/01", "2016/02/02"];
 
 var_dump($soup); //lets take a look at the object
 
-$print $soup->name . "is the soup we are having.<br />";
+print $soup->name . "is the soup we are having.<br />";
 
 /*this object is a very simple standard class object. 
 in reality, you may want to just create an associative array.
